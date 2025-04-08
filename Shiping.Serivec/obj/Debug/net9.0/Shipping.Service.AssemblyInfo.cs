78b3c3e4a0bfd950427b25f441b2c688d531537b
AmanyMousa <01002607229mn@email.com>
2025-04-08 14:09:08 +0200

    okk

--- Shiping.Serivec/obj/Debug/net9.0/Shipping.Service.AssemblyInfo.cs
+++ Shiping.Serivec/obj/Debug/net9.0/Shipping.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb25c9b8f93079ddcfd6e90d470c2d7a007be0ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7df19e6e82c2ed531adcd17735db1209985dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
