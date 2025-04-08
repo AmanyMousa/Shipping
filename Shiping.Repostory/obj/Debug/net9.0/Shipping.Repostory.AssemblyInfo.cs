45de9294c70b4591cdb7261ef21e9d959964624a
AmanyMousa <01002607229mn@email.com>
2025-04-08 14:40:36 +0200

    addddddddddddddddddddd

--- Shiping.Repostory/obj/Debug/net9.0/Shipping.Repostory.AssemblyInfo.cs
+++ Shiping.Repostory/obj/Debug/net9.0/Shipping.Repostory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping.Repostory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97225533b1571786a0af6073dfc16f069818b3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc50d4c820f0aa6398bc44e45bf323e8222cfaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping.Repostory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping.Repostory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
